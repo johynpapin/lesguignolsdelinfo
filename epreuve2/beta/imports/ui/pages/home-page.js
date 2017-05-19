05c1b79a1feb7374cd4c1c34eaa14010bf470962
Johyn Papin <johynpapin@COMPUT-1.localdomain>
2017-05-20 01:14:07 +0200

    test

--- epreuve2/beta/imports/ui/pages/home-page.js
+++ epreuve2/beta/imports/ui/pages/home-page.js
@@ -1,14 +1,1 @@
 import './home-page.html';
-
-Template.home-page.onRendered(function() {
-  let settings = 'pjs-settings.json';
-  this.autorun(() => {
-    if (particlesJS) {
-      console.log(`loading particles.js config from "${settings}"...`)
-      /* particlesJS.load(@dom-id, @path-json, @callback (optional)); */
-      particlesJS.load('particles-js', settings, function () {
-        console.log('callback - particles.js config loaded');
-      });
-    }
-  });
-});
