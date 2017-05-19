f370a51e56f47dc965f6e50cb3413b43d883713a
Quentin RIBAC <ribac.quentin@gmail.com>
2017-05-19 16:41:39 +0200

    board matrix + sample player

--- epreuve1/index.js
+++ epreuve1/index.js
@@ -5,6 +5,8 @@ const client = net.connect({host: '127.0.0.1', port: 1337}, () => {
 	client.write('LesGuignolsDeLInfo');
 });
 
+let b = [];
+let players = [];
 client.on('data', data => {
 	data = data.toString().slice(0, -1);
 	if (data.length === 1) {
@@ -17,9 +19,44 @@ client.on('data', data => {
 		data = data.split('/');
 		const x = Number(data[0].split('x')[0]);
 		const y = Number(data[0].split('x')[1]);
+		data[1] = data[1].split('-');
+		for (let i = 0; i < x; i++) {
+			b[i] = data[1].slice(i * y, (i + 1) * y);
+			b[i] = b[i].map(val => {
+				return (Number(val) == val) ? Number(val) : val;
+			});
+		}
+		players = data[2].substr(2).split('-').map(xy => {
+			xy = xy.split(',');
+			return {x: xy[0], y: xy[1]};
+		});
 	}
 });
 
 client.on('end', () => {
 	console.log('disconnected from server');
 });
+
+let player = {
+	x: 0,
+	y: 0,
+	nbF: 0,
+	nbB: 0,
+	canWalkOn: function (xPos, yPos, b) {
+		if (['S', 'B', 'F'].indexOf(b[xPos][yPos]) !== -1) {
+			//sand, beer or fries
+			return true;
+		} else if (Number(b[xPos][yPos]) == b[xPos][yPos]) {
+			//number (mussel)
+			return true;
+		} else if ((b[xPos][yPos] === 'D') && (nbF > 0) {
+			//next is a dune and we got fries. can we walk on what’s on the other side?
+			let otherSide = b[xPos + (xPos - this.x)][yPos + (yPos - this.y)];
+			if ((['S', 'B', 'F'].indexOf(otherSide) !== -1) || (Number(otherSide) == otherSide)) {
+				return true;
+			}
+		}
+		return false;
+	}
+};
+
