8b0aab9dcb8772a9a3c4461e0fb3988ecbc5fa2c
Johyn Papin <johynpapin@COMPUT-1.localdomain>
2017-05-19 15:02:46 +0200

    Test

--- epreuve1/index.js
+++ epreuve1/index.js
@@ -5,8 +5,23 @@ const client = net.connect({host: '127.0.0.1', port: 1337}, () => {
 	client.write('world!\r\n');
 });
 
-client.on('data', (data) => {
+let b = { // Board
+	size: {
+		x: 0,
+		y: 0
+	}
+}
+
+client.on('data', data => {
+	data = data.toString();
+	if (data === 'FIN') {
+		console.log('End of the round');
+	}
 	console.log(data.toString());
+	data = data.toString().split('/');
+	b.size.x = Number(data[0].split('x')[0]);
+	b.size.y = Number(data[0].split('x')[1]);
+	console.log(b);
 	client.end();
 });
 
