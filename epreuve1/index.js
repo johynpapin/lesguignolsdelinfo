6237a983a1fb7e4ea2ea6a9c14721303df236405
Johyn Papin <johynpapin@COMPUT-1.localdomain>
2017-05-19 15:47:03 +0200

    42

--- epreuve1/index.js
+++ epreuve1/index.js
@@ -2,27 +2,22 @@ const net = require('net');
 
 const client = net.connect({host: '127.0.0.1', port: 1337}, () => {
 	console.log('connected to server!');
-	client.write('world!\r\n');
+	client.write('LesGuignolsDeLInfo');
 });
 
-let b = { // Board
-	size: {
-		x: 0,
-		y: 0
-	}
-}
-
 client.on('data', data => {
-	data = data.toString();
-	if (data === 'FIN') {
+	data = data.toString().slice(0, -1);
+	if (data.length === 1) {
+		b.number = Number(data);
+		console.log('We are the player no ' + b.number);
+	} else if (data === 'FIN') {
 		console.log('End of the round');
+	} else {
+		console.log('test')
+		data = data.split('/');
+		const x = Number(data[0].split('x')[0]);
+		const y = Number(data[0].split('x')[1]);
 	}
-	console.log(data.toString());
-	data = data.toString().split('/');
-	b.size.x = Number(data[0].split('x')[0]);
-	b.size.y = Number(data[0].split('x')[1]);
-	console.log(b);
-	client.end();
 });
 
 client.on('end', () => {
