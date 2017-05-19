e5af9d6097254cb9f8fd9008f194d1a12bfda0d4
Johyn Papin <johynpapin@COMPUT-1.localdomain>
2017-05-19 23:50:05 +0200

    salut

--- epreuve2/beta/imports/startup/client/routes.js
+++ epreuve2/beta/imports/startup/client/routes.js
@@ -1,8 +1,15 @@
 import '../../ui/layouts/main-layout';
 import '../../ui/pages/home-page';
+import '../../ui/pages/timeline-page';
 
 FlowRouter.route('/', {
   action: function() {
     BlazeLayout.render('main-layout', {content: 'home-page'});
+  }
+});
+
+FlowRouter.route('/timeline', {
+  action: function() {
+    BlazeLayout.render('main-layout', {content: 'timeline-page'});
   }
 });
