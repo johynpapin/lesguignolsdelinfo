70325bb1be5c9ab0abbdc21206fa90ddb641569d
Johyn Papin <johynpapin@COMPUT-1.localdomain>
2017-05-19 17:52:06 +0200

    42

--- epreuve1/graph.js
+++ epreuve1/graph.js
@@ -7,11 +7,11 @@ module.exports = exports = class Graph {
 		let neighbors = [];
 		if (node.x > 1 && this.nodes[node.x - 1][node.y] !== 'D')
 			neighbors.push({x: node.x - 1, y: node.y});
-		if (node.x < this.nodes[0].length - 1 && this.nodes[node.x + 1][node.y] !== 'D')
+		if (node.x < this.nodes.length - 1 && this.nodes[node.x + 1][node.y] !== 'D')
 			neighbors.push({x: node.x + 1, y: node.y});
 		if (node.y > 1 && this.nodes[node.x][node.y - 1] !== 'D')
 			neighbors.push({x: node.x, y: node.y - 1});
-		if (node.y < this.nodes.length - 1 && this.nodes[node.x][node.y + 1] !== 'D')
+		if (node.y < this.nodes[0].length - 1 && this.nodes[node.x][node.y + 1] !== 'D')
 			neighbors.push({x: node.x, y: node.y + 1});
 		return neighbors;
 	}
