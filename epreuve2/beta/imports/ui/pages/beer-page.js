0348b4b57d2256b1a0804184b320c72d0a3df93a
Quentin RIBAC <ribac.quentin@gmail.com>
2017-05-20 04:35:36 +0200

    bp

---
+++ epreuve2/beta/imports/ui/pages/beer-page.js
@@ -0,0 +1,8 @@
+import { Template } from 'meteor/templating';
+
+import { Beers } from '/imports/api/beers/beers';
+import { Comments } from '/imports/api/comments/comments';
+
+import '../components/beer';
+import './beer-page.html';
+
