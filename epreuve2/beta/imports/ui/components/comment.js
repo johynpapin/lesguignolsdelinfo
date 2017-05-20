7cc5210cc55264e468feded281a8ab02a25c68f6
Quentin RIBAC <ribac.quentin@gmail.com>
2017-05-20 05:06:07 +0200

    yolo

---
+++ epreuve2/beta/imports/ui/components/comment.js
@@ -0,0 +1,11 @@
+import { Template } from 'meteor/templating';
+import { Meteor } from 'meteor/meteor';
+
+import './comment.html';
+
+Template.comment.events({
+})
+
+Template.comment.helpers({
+});
+
