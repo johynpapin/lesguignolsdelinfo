49d973d2a67e23cf9d0d14bef8a0bbdb069d1e97
Johyn Papin <johynpapin@COMPUT-1.localdomain>
2017-05-20 02:25:45 +0200

    register

--- epreuve2/beta/imports/ui/components/register-form.js
+++ epreuve2/beta/imports/ui/components/register-form.js
@@ -1,1 +1,17 @@
+import { Template } from 'meteor/templating';
+
 import './register-form.html';
+
+Template.registerForm.helpers({
+	nextDisabled() {
+		return true;
+	}
+});
+
+Template.registerForm.events({
+	'change input'(e) {
+		const firstName = $('#r-first-name').val();
+		const lastName = $('#r-last-name').val();
+		const email = $('#r-email').val();
+	}
+});
