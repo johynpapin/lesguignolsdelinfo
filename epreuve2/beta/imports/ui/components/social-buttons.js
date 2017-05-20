a82aa387fddbd652ecf99be9d77ead18bc6ef72f
Johyn Papin <johynpapin@COMPUT-1.localdomain>
2017-05-20 02:04:04 +0200

    test

--- epreuve2/beta/imports/ui/components/social-buttons.js
+++ epreuve2/beta/imports/ui/components/social-buttons.js
@@ -1,10 +1,1 @@
 import './social-buttons.html';
-
-<div id="fb-root"></div>
-<script>(function(d, s, id) {
-  var js, fjs = d.getElementsByTagName(s)[0];
-  if (d.getElementById(id)) return;
-  js = d.createElement(s); js.id = id;
-  js.src = "//connect.facebook.net/fr_FR/sdk.js#xfbml=1&version=v2.9";
-  fjs.parentNode.insertBefore(js, fjs);
-}(document, 'script', 'facebook-jssdk'));</script>
