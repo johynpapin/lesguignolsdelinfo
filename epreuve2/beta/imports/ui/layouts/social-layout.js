9606ebb3091303985d1bf5abf50a90245c0490fc
Johyn Papin <johynpapin@COMPUT-1.localdomain>
2017-05-20 04:16:02 +0200

    test

--- epreuve2/beta/imports/ui/layouts/social-layout.js
+++ epreuve2/beta/imports/ui/layouts/social-layout.js
@@ -4,12 +4,15 @@ import './social-layout.html';
 
 Template.socialLayout.helpers({
 	avatarUrl() {
-		return Gravatar.imageUrl(Meteor.user().emails[0].address);
+		if (Meteor.user())
+			return Gravatar.imageUrl(Meteor.user().emails[0].address);
 	},
 	fullName() {
-		return Meteor.user().profile.firstName + ' ' + Meteor.user().profile.lastName;
+		if (Meteor.user())
+			return Meteor.user().profile.firstName + ' ' + Meteor.user().profile.lastName;
 	},
 	email() {
-		return Meteor.user().emails[0].address;
+		if (Meteor.user())
+			return Meteor.user().emails[0].address;
 	}
 })
--- epreuve2/beta/imports/ui/pages/beers-page.js
+++ epreuve2/beta/imports/ui/pages/beers-page.js
@@ -2,7 +2,8 @@ import { Template } from 'meteor/templating';
 
 import { Beers } from '/imports/api/beers/beers';
 
-import '../components/insert-beer-modal.js';
+import '../components/insert-beer-modal';
+import '../components/beer';
 
 import './beers-page.html';
 
