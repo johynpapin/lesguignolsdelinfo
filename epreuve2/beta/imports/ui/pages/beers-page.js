d0bd365cebc41e65b84d9b7d10dadfe17fde94b3
Johyn Papin <johynpapin@COMPUT-1.localdomain>
2017-05-20 04:11:33 +0200

    test

--- epreuve2/beta/imports/ui/pages/beers-page.js
+++ epreuve2/beta/imports/ui/pages/beers-page.js
@@ -1,3 +1,14 @@
+import { Template } from 'meteor/templating';
+
+import { Beers } from '/imports/api/beers/beers';
+
 import '../components/insert-beer-modal.js';
 
 import './beers-page.html';
+
+
+Template.beersPage.helpers({
+	beers() {
+		return Beers.find({});
+	}
+})
